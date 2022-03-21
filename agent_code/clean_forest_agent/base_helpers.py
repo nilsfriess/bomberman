6d0dbc3a471007976028708c93956a3298102989
Nils Friess <nils.friess@gmail.com>
2022-03-21 23:41:04 +0100

    Merge my agent into base branch

---
+++ agent_code/clean_forest_agent/base_helpers.py
@@ -0,0 +1,162 @@
+import pyastar2d
+import numpy as np
+from scipy.spatial.distance import cdist
+
+from settings import ROWS, COLS
+
+from .bomb_helpers import should_drop_bomb
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+
+coords = [[i,j] for i in range(ROWS) for j in range(COLS)]
+def find_path(field, start, goal):
+    start = np.array(start)
+    weights = cdist(coords, [start], 'cityblock').reshape(ROWS, COLS).astype(np.float32)
+    
+    weights = weights + 1 # weights must be >= 1
+    weights[field != 0] = 10e9 # walls have very weight
+
+    # Set field at goal to zero if we are looking for something on the field
+    # Otherwise the algorithm would not find a path to the target.
+    weights[goal] = 1
+    
+    # Compute shortest path from start to goal using A*
+    path = pyastar2d.astar_path(weights, start, goal, allow_diagonal=False)
+    if path is None:
+        return []
+    return path[1:] # discard first element in path, since it's the start position
+
+
+'''
+Computes for each asset in `assets` the shortest path from `self_pos`
+to `asset` using A*.
+
+Returns the first coordinate of the shortest among all paths.
+'''
+def find_next_step_to_assets(field, others, self_pos, assets):
+    if len(assets) == 0:
+        return self_pos
+
+    for other in others:
+        field[other] = 1
+    
+    shortest_path_length = float("inf")
+    best_coord = self_pos
+    
+    for asset in assets:
+        path = find_path(field, self_pos, asset)
+
+        if (len(path) > 0) and (len(path) < shortest_path_length):
+            shortest_path_length = len(path)
+            best_coord = path[0]
+
+    return best_coord
+
+'''
+Returns 1-hot encoded 4-array where `self_pos` and `asset_pos`
+are two neighboring coordinates on the grid. Each component
+of the result corresponds to one direction, i.e., one component
+is 1 iff `self_pos` is directly above `asset_pos`, one component
+is 1 iff `self_pos` is left of `asset_pos` etc. 
+
+Should be used together with `find_next_step_to_assets`
+'''
+def direction_from_coordinates(self_pos, asset_pos):
+    direction = np.zeros((4,1))
+
+    self_pos = np.array(self_pos)
+    asset_pos = np.array(asset_pos)
+    
+    if not np.array_equal(self_pos, asset_pos):
+        dist = self_pos - asset_pos
+
+        if dist[0] == 0:
+            if dist[1] == 1:
+                direction[0] = 1
+            else:
+                direction[1] = 1
+        else:
+            if dist[0] == 1:
+                direction[2] = 1
+            else:
+                direction[3] = 1
+
+    return direction
+
+def action_from_direction(direction):
+    if direction[0] == 1:
+        return 'UP'
+
+    if direction[1] == 1:
+        return 'DOWN'
+
+    if direction[2] == 1:
+        return 'LEFT'
+
+    if direction[3] == 1:
+        return 'RIGHT'
+
+    return 'WAIT'
+
+def compute_risk_map(game_state):
+    '''
+    Compute a 'risk factor' for each tile of the game map.
+    For instance, if a bomb is about to explode and a tile
+    is within the explosion radius, the tile has a high risk.
+    Free tiles or tiles with coins have low risk.
+    '''
+    field = game_state['field']
+    self_pos = game_state['self'][3]
+    
+    # Explosions are definitely high-risk regions
+    risk_map = np.array(game_state['explosion_map'], dtype=int)
+    risk_map[risk_map != 0] = 100
+    risk_map[field != 0] = 200
+
+    # Below, we subtract the number of available "escape squares" from the risk.
+    # Walking to a square with only one escape square is very risk, if many
+    # escape squares are available, the agent has different escape paths to
+    # choose from.
+    _, n_escape_squares = should_drop_bomb(game_state)
+    
+    for bomb_pos, bomb_val in game_state['bombs']:
+        if (abs(bomb_pos[0] - self_pos[0]) + abs(bomb_pos[1] - self_pos[1])) >= 6:
+            # Bomb not near us, check next bomb
+            continue
+
+        bomb_risk = 100 - 10*bomb_val   # Bombs are riskier, the lower their timer is
+        risk_map[bomb_pos] = 100
+        
+        for k in [-1,-2,-3]: # Left of bomb
+            coord_on_field = (bomb_pos[0] + k, bomb_pos[1])
+
+            if field[coord_on_field] == -1:
+                # Reached a wall, no need to look further
+                break
+            risk_map[coord_on_field] = bomb_risk - abs(k) - n_escape_squares['LEFT']
+
+        for k in [1,2,3]: # Right of bomb
+            coord_on_field = (bomb_pos[0] + k, bomb_pos[1])
+
+            if field[coord_on_field] == -1:
+                # Reached a wall, no need to look further
+                break
+            risk_map[coord_on_field] = bomb_risk - abs(k) - n_escape_squares['RIGHT']
+
+        for k in [-1,-2,-3]: # Above bomb
+            coord_on_field = (bomb_pos[0], bomb_pos[1]+k)
+
+            if field[coord_on_field] == -1:
+                # Reached a wall, no need to look further
+                break
+            risk_map[coord_on_field] = bomb_risk - abs(k) - n_escape_squares['UP']
+
+        for k in [1,2,3]: # Below bomb
+            coord_on_field = (bomb_pos[0], bomb_pos[1]+k)
+
+            if field[coord_on_field] == -1:
+                # Reached a wall, no need to look further
+                break
+            risk_map[coord_on_field] = bomb_risk - abs(k) - n_escape_squares['DOWN']
+            
+    return risk_map
---
+++ agent_code/clean_forest_agent/callbacks.py
@@ -0,0 +1,47 @@
+import os
+from pickle import load 
+
+import numpy as np
+
+from .estimator import GBTEstimator
+from .state_action_helpers import generate_stupid_actions, random_action, generate_suicidal_actions
+from .base_helpers import ACTIONS
+
+def setup(self):
+    self.epsilon = 0.4
+    self.learning_rate = 0.2
+    self.discount_factor = 0.8
+
+    self.initial_epsilon = self.epsilon
+    self.initial_learning_rate = self.learning_rate
+
+    self.action_filter_prob = 1
+    
+    self.estimator = GBTEstimator(0.1, self.discount_factor)
+
+    if os.path.isfile("models/model.pt"):
+        with open("models/model.pt", "rb") as file:
+            self.estimator = load(file)
+            print("LOADED MODEL")
+
+def act(self, game_state):
+    if np.random.uniform() < 1 - self.action_filter_prob: # Only filter sometimes
+        stupid_actions = []
+    else:
+        stupid_actions = generate_stupid_actions(game_state)
+
+    suicidal_actions = generate_suicidal_actions(game_state)
+
+    stupid_actions = suicidal_actions | set(stupid_actions)
+    
+    if np.random.uniform() < 1-self.epsilon:
+        action = self.estimator.estimate(game_state)
+        
+        while action in stupid_actions:
+            action = random_action()
+    else:
+        action = random_action()
+        while action in stupid_actions:
+            action = random_action()
+
+    return action
